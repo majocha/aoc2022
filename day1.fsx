fe7fe563a802e0453cf0c65351d0c1f1de450eea
Jakub Majocha <majocha@outlook.com>
2022-12-01 18:19:31 +0100

    first commit

---
+++ day1.fsx
@@ -0,0 +1,22 @@
+#r "nuget: FSharpPlus, 1.2.5"
+open FSharpPlus
+
+let input = System.IO.File.ReadAllLines "1.txt"
+let elves = input |> Seq.split [ [""] ]
+let sumOne a: int = a |> map (sscanf "%d") |> sum
+elves |> map sumOne |> Seq.sortDescending |> take 3 |> sum
+
+
+let elves2 = 
+    [
+        let mutable cals = 0
+        for s in input do
+            match tryParse s with
+            | Some n -> cals <- cals + n
+            | _ ->
+                yield cals
+                cals <- 0
+        yield cals
+    ]
+
+elves2 |> List.sortDescending |> take 3 |> sum
