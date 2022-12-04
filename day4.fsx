2f1122f1f055d1d6a9a61d0495d93898456a9977
Jakub Majocha <majocha@outlook.com>
2022-12-04 07:47:47 +0100

    simpler

--- day4.fsx
+++ day4.fsx
@@ -1,17 +1,15 @@
 #r "nuget: FSharpPlus, 1.2.5"
 open FSharpPlus
-let sections (x1, x2, y1 ,y2) = Set [x1..x2], Set [y1..y2]
-let input = System.IO.File.ReadAllLines "4.txt" |> map (sscanf "%d-%d,%d-%d") |> map sections
 
-let fully sets =
-     sets ||> Set.intersect = fst sets
+let input = System.IO.File.ReadAllLines "4.txt" |> map (sscanf "%d-%d,%d-%d")
 
-let isMatch (a, b) =
-    fully (a, b) || fully (b, a)
+let fully (x1, x2, y1 ,y2) =
+    (y1 <= x1 && x2 <= y2) || (x1 <= y1 && y2 <= x2)
 
-input |> filter isMatch |> length
+input |> filter fully |> length
 
-let anyOverlap sets =
-    sets ||> Set.intersect |> Set.count > 0
+// part two
+let anyOverlap (x1, x2, y1, y2) =
+    if x1 < y1 then x2 >= y1 else y2 >= x1
 
 input |> filter anyOverlap |> length
