a5fb9785629101c583229d62e5433c733f728573
Jakub Majocha <majocha@outlook.com>
2022-12-11 12:46:46 +0100

    part one

--- day11.fsx
+++ day11.fsx
@@ -1,8 +1,8 @@
 open System.IO
 open System.Text.RegularExpressions
 
-type Inspect = int -> int * int
-type Monkey = {Id: int; Items: int list; Inspect: Inspect; Business: int}
+type Inspect = int64 -> int * int64
+type Monkey = {Id: int; Items: int64 list; Inspect: Inspect; Business: int}
 let parseMonkey text =
     printfn $"{text}"
     let number name = $"(?<{name}>\d+)"
@@ -19,25 +19,25 @@ let parseMonkey text =
         let matched = Regex.Match(text, pattern, RegexOptions.Multiline)
         matched.Groups[key].Value
     let parsedi = parsed >> int
-    let items = [ for i in (parsed "items").Split ',' -> int i ]
+    let items = [ for i in (parsed "items").Split ',' -> int64 i ]
     let monkeyId = parsedi "mid"
-    let divisibleBy = parsedi "divisibleBy"
+    let divisibleBy = parsedi "divisibleBy" |> int64
     let ifTrue = parsedi "ifTrue"
     let ifFalse = parsedi "ifFalse"
 
-    let inspect = fun worryLevel ->
+    let inspect = fun (worryLevel: int64) ->
         let p =
            match parsed "p" with "old" -> worryLevel | _ -> parsedi "p"
+           |> int64
         let afterWorryLevel =       
             match parsed "op" with 
-            | "*" -> (worryLevel * p) / 3
-            | "+" -> (worryLevel + p) / 3
+            | "*" -> (worryLevel * p) / 3L
+            | "+" -> (worryLevel + p) / 3L
             | _ -> failwith "error"
-        let throwTo = if afterWorryLevel % divisibleBy = 0 then ifTrue else ifFalse
+        let throwTo = if afterWorryLevel % divisibleBy = 0L then ifTrue else ifFalse
         throwTo, afterWorryLevel
 
     {Id = monkeyId; Items = items; Inspect = inspect; Business = 0}
-
 
 let monkeys = 
   File.ReadAllLines "11.txt" |> Seq.chunkBySize 7 |> Seq.map (Seq.take 6 >> String.concat "\n")    //|> Array.map parseMonkey
@@ -45,19 +45,18 @@ let monkeys =
 
 let turn (monkeys: Monkey list) monkeyId =
   let monkey = monkeys[monkeyId]
-  let groups = monkey.Items |> List.map monkey.Inspect |> List.groupBy fst |> Map
+  let thrown = monkey.Items |> List.map monkey.Inspect |> List.groupBy fst |> Map
+  let thrownTo i = thrown |> Map.tryFind i |> Option.defaultValue [] |> List.map snd
   [
-    for {Id = i; Items = items} as m in monkeys do
-      let thrown = groups |> Map.tryFind i |> Option.defaultValue [] |> List.map snd
+    for {Id = i; Items = items} as m in monkeys do 
       if i = monkeyId then
-        { m with Items = thrown; Business = m.Business + items.Length }
+        { m with Items = []; Business = m.Business + items.Length }
       else
-        { m with Items = items @ thrown }
+        { m with Items = items @ thrownTo i }
   ]
 
-let rec round (monkeys: Monkey list) = function
-  | 0 -> monkeys
-  | n ->
-    round ([0..monkeys.Length - 1] |> List.fold turn monkeys) (n - 1)
+let round (monkeys: Monkey list) _ =
+  [0..monkeys.Length - 1] |> List.fold turn monkeys
 
-round monkeys 20 |> List.map (fun { Business = b } -> b) |> List.sort |> List.rev |> List.take 2 |> List.reduce (*)
+[1..20] |> List.fold round monkeys
+|> List.map (fun { Business = b } -> b) |> List.sort |> List.rev |> List.take 2 |> List.reduce (*)
