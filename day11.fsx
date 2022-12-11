4229549d6a61e5a289d5878391b45ab825656f7d
Jakub Majocha <majocha@outlook.com>
2022-12-11 23:04:36 +0100

    day 11 part two

--- day11.fsx
+++ day11.fsx
@@ -1,47 +1,58 @@
 open System.IO
 open System.Text.RegularExpressions
 
-type Inspect = int64 -> int * int64
-type Monkey = {Id: int; Items: int64 list; Inspect: Inspect; Business: int}
+// basically map of remainders
+type Primed = Primed of Map<int, int>
+module Primed =
+  let private primes = [2; 3; 5; 7; 11; 13; 17; 19; 23]
+  let ofInt n =
+    [ for p in primes  -> p, n % p ] |> Map |> Primed
+  let multiply (Primed rs1) (Primed rs2) =
+    [ for p in primes -> p, (rs1[p] * rs2[p]) % p ] |> Map |> Primed
+  let add (Primed rs1) (Primed rs2) =
+    [ for p in primes -> p, (rs1[p] + rs2[p]) % p ] |> Map |> Primed
+  let isDivisibleBy p (Primed rs) = rs[p] = 0
+
+type Inspect = Primed -> int * Primed
+type Monkey = {Id: int; Items: Primed list; Inspect: Inspect; Business: int64}
+
 let parseMonkey text =
-    printfn $"{text}"
-    let number name = $"(?<{name}>\d+)"
-    let toLineEnd name = $"(?<{name}>.*)"
-    let operation name = $"(?<{name}>\*|\+|)"
-    let operand name = $"(?<{name}>\d+|old)"
-    let pattern = $"""Monkey {number "mid"}:
-  Starting items: {toLineEnd "items"}
-  Operation: new = old {operation "op"} {operand "p"}
-  Test: divisible by {number "divisibleBy"}
-    If true: throw to monkey {number "ifTrue"}
-    If false: throw to monkey {number "ifFalse"}"""
+
+    // this multi line pattern will not match when this file or input is in CRLF format
+    let pattern = """Monkey (?<mid>\d+):
+  Starting items: (?<items>.*)
+  Operation: new = old (?<operation>\*|\+|) (?<operand>\d+|old)
+  Test: divisible by (?<divisibleBy>\d+)
+    If true: throw to monkey (?<ifTrue>\d+)
+    If false: throw to monkey (?<ifFalse>\d+)"""
+
     let parsed (key: string) =
         let matched = Regex.Match(text, pattern, RegexOptions.Multiline)
         matched.Groups[key].Value
+      
     let parsedi = parsed >> int
-    let items = [ for i in (parsed "items").Split ',' -> int64 i ]
+
+    let items = [ for i in (parsed "items").Split ',' -> i |> int |> Primed.ofInt ]
     let monkeyId = parsedi "mid"
-    let divisibleBy = parsedi "divisibleBy" |> int64
+    let operation = parsed "operation"
+    let primeToDivideBy = parsedi "divisibleBy"
     let ifTrue = parsedi "ifTrue"
     let ifFalse = parsedi "ifFalse"
 
-    let inspect = fun (worryLevel: int64) ->
+    let inspect = fun worryLevel ->
         let p =
-           match parsed "p" with "old" -> worryLevel | _ -> parsedi "p"
-           |> int64
+           match parsed "operand" with "old" -> worryLevel | _ -> parsedi "operand" |> Primed.ofInt
         let afterWorryLevel =       
-            match parsed "op" with 
-            | "*" -> (worryLevel * p) / 3L
-            | "+" -> (worryLevel + p) / 3L
+            match operation with 
+            | "*" -> Primed.multiply worryLevel p
+            | "+" -> Primed.add worryLevel p
             | _ -> failwith "error"
-        let throwTo = if afterWorryLevel % divisibleBy = 0L then ifTrue else ifFalse
+        let throwTo = if  afterWorryLevel |> Primed.isDivisibleBy primeToDivideBy then ifTrue else ifFalse
         throwTo, afterWorryLevel
 
-    {Id = monkeyId; Items = items; Inspect = inspect; Business = 0}
+    { Id = monkeyId; Items = items; Inspect = inspect; Business = 0 }
 
-let monkeys = 
-  File.ReadAllLines "11.txt" |> Seq.chunkBySize 7 |> Seq.map (Seq.take 6 >> String.concat "\n")    //|> Array.map parseMonkey
-  |> Seq.map parseMonkey |> Seq.toList
+let monkeys = (File.ReadAllText "11.txt").Split "\n\n" |> Seq.map parseMonkey |> Seq.toList
 
 let turn (monkeys: Monkey list) monkeyId =
   let monkey = monkeys[monkeyId]
@@ -50,7 +61,7 @@ let turn (monkeys: Monkey list) monkeyId =
   [
     for {Id = i; Items = items} as m in monkeys do 
       if i = monkeyId then
-        { m with Items = []; Business = m.Business + items.Length }
+        { m with Items = []; Business = m.Business + int64 items.Length }
       else
         { m with Items = items @ thrownTo i }
   ]
@@ -58,5 +69,6 @@ let turn (monkeys: Monkey list) monkeyId =
 let round (monkeys: Monkey list) _ =
   [0..monkeys.Length - 1] |> List.fold turn monkeys
 
-[1..20] |> List.fold round monkeys
-|> List.map (fun { Business = b } -> b) |> List.sort |> List.rev |> List.take 2 |> List.reduce (*)
+let partTwo = 
+  [1..10000] |> List.fold round monkeys
+  |> List.map (fun { Business = b } -> b) |> List.sort |> List.rev |> List.take 2 |> List.reduce (*)
