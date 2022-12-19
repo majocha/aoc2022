936b5f0ef0313b0520c6b8d92979191cd9e26f40
majocha <majocha@outlook.com>
2022-12-19 21:33:24 +0100

    cleanup

--- day17.fsx
+++ day17.fsx
@@ -1,6 +1,3 @@
-#r "nuget:FSharpPlus"
-open FSharpPlus
-
 let blocks = 
     let toInt (data: int array) =
         [0..3] |> List.fold (fun b i -> (b <<< 8) ^^^ data[i]) 0
@@ -41,13 +38,15 @@ let blocks =
             0b1100
             0b1100
         |]
-    |] |> map toInt
+    |] |> Array.map toInt
 
-type CircularBuffer(bufferLength) =
-    let a = Array.create bufferLength 0uy
-    member _.Item
-        with get (y: int64) = a[int (if y >= 0 then y % int64 bufferLength else  (int64 bufferLength + y % int64 bufferLength - 1L))]
-        and set (y: int64) v = a[int (if y >= 0 then y % int64 bufferLength else (int64 bufferLength + y % int64 bufferLength - 1L))] <- v
+let inline circular length i =
+    if i >= 0L then i % length else length + i % length - 1L
+    |> int
+
+type CircularBuffer(length) =
+    let a = Array.create length 0uy
+    member _.Item with get i = a[circular length i]and set i v = a[circular length i] <- v
 
 let jets =
     System.IO.File.ReadAllText "17.txt"
@@ -56,11 +55,13 @@ let jets =
 
 let inline hitsWall dx segment =
     (segment >>> dx) &&& 0b10000000uy = 0b10000000uy|| (segment >>> (dx - 1)) &&& 0b1uy = 0b1uy
+
 let inline hitsRocks rockLayer dx segment = rockLayer &&& (segment >>> dx) <> 0uy
+
 let inline getSegment n block = (block >>> (n * 8)) |> byte
 
 let blocked (rocks: CircularBuffer) block dx y =
-    [0..3] |> exists (fun n ->
+    [0..3] |> List.exists (fun n ->
         let segment = getSegment n block
         (hitsRocks (rocks[y + int64 n]) dx segment) || (hitsWall dx segment)
     )
@@ -80,7 +81,6 @@ let rec drop (rocks: CircularBuffer) jetn block x y =
     else drop rocks nextJetn block x (y - 1L)
 
 let dropSeq (jetn, layers) y startN takeEvery =
-
     let rocks = CircularBuffer(512)
     for i = 0 to (layers |> Array.length) - 1 do
         rocks[y - int64 i] <- layers[i]
@@ -103,12 +103,14 @@ let dropSeq (jetn, layers) y startN takeEvery =
     step y jetn startN
 
 #time "on"
+let result = Seq.take 1 >> Seq.map snd >> Seq.head
 
-let partOne = (dropSeq (0, [| 127uy |]) 0L 0L 2022L) |> skip 1 |> take 1 |> map snd |> toList
+let partOne = (dropSeq (0, [| 127uy |]) 0L 0L 2022L) |> Seq.skip 1 |> result
 
-let zeros = (dropSeq (0, [| 127uy |]) 0L 0L 5L) |> take 100_000 |> toList
-let magic = zeros |> Seq.countBy fst |> sortBy snd |> find (fun (j, c) -> c > 1) |> fst
-let ns, ys = zeros |> filter (fun (p, y) -> p = magic) |> map snd |> toList |> unzip
+let zeros = (dropSeq (0, [| 127uy |]) 0L 0L 5L) |> Seq.take 100_000 |> Seq.toList
+let magic = zeros |> Seq.countBy fst |> Seq.sortBy snd |> Seq.find (fun (j, c) -> c > 1) |> fst
+let repeating f = zeros |> Seq.filter (fun (p, _) -> p = magic) |> Seq.map snd |> Seq.map f |> Seq.take 3 |> Seq.toArray
+let ns, ys = repeating fst, repeating snd
 let startsAtN, repeatsEveryN = ns[0], ns[2] - ns[1]
 let startsAt, repeatsEvery = ys[0], ys[2] - ys[1]
 
@@ -116,6 +118,4 @@ let cycles = (1000000000000L - startsAtN) / repeatsEveryN
 let y = startsAt + cycles * repeatsEvery
 let n = startsAtN + cycles * repeatsEveryN
 
-dropSeq magic (startsAt) (startsAtN) 2022L |> take 1 |> map snd |> head //|> head
-
-let partTwo = dropSeq magic y n 1000000000000L |> take 1 |> map snd |> head
+let partTwo = dropSeq magic y n 1000000000000L |> result
