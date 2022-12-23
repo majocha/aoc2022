f75151a3d4abccfb9beb174ef84de38f8ee16d90
majocha <majocha@outlook.com>
2022-12-23 23:14:27 +0100

    part two

--- day23.fsx
+++ day23.fsx
@@ -9,7 +9,14 @@ let sizeX = input |> head |> length
 
 type D = N | S | W | E
 type Elf = int * int
+type Proposition = Elf * Elf
 
+let elves =
+    [
+        for x in 0..sizeX - 1 do
+            for y in 0 .. sizeY - 1 do
+                if input[y][x] = '#' then (x, y)
+    ] |> Set
 
 let inline (=>) (x, y) (dx, dy) = x + dx, y + dy
 
@@ -18,7 +25,7 @@ let allAdj e =
         for x in -1 .. 1 do
             for y in -1 .. 1 do
                 if (x, y) <> (0, 0) then e => (x, y)
-    ] |> Set
+    ]
 
 let adj e = function
     | N -> [for x in -1 .. 1 do e => (x, -1)]
@@ -28,69 +35,49 @@ let adj e = function
 
 let move d e = adj e d |> item 1
 
-let elves =
-    [
-        for x in 0..sizeX - 1 do
-            for y in 0 .. sizeY - 1 do
-                if input[y][x] = '#' then (x, y)
-    ] |> Set
-
-
-type Proposition = Proposition of Elf * Elf
-
 let propose (elves: Set<Elf>) (dirs: D list) (e: Elf) =
-    let good d = Set.intersect elves (Set (adj e d)) |> Set.isEmpty
-    if elves |> Set.intersect (allAdj e) |> Set.isEmpty then
-        Proposition (e, e)
-    else
+    let good d = (adj e d) |> forall (fun p -> elves |> Set.contains p |> not)
+    if allAdj e |> exists (fun p -> elves |> Set.contains p) then
         dirs |> tryFind good 
         |> Option.map(fun d -> Proposition (e, move d e))
         |> Option.defaultValue (Proposition (e, e))
+    else Proposition (e, e)
 
 let firstHalf dirs elves = elves |> map (propose elves dirs)
 
-let sameDest p1 p2 =
-    let (Proposition(_, e1)) = p1
-    let (Proposition(_, e2)) = p2
-    e1 = e2
+let secondHalf (ps: Set<Proposition>) = 
+    let bad = 
+        ps |> groupBy snd |> map (fun (d, ps) -> d, ps |> length)
+        |> filter ( fun (d, l) -> l > 1) |> map fst |> Set
+    ps |> Set.map ( fun (current, next) -> if bad |> Set.contains next then current else next)
 
-let secondHalf propositions =
-    [|
-        for (Proposition(prev, next) as p) in propositions do
-            if propositions |> Set.remove p |> Set.exists (sameDest p) then
-                prev
-            else 
-                next
-    |] |> Set
+module PartOne =
 
-let extent elves =
-    let xs = elves |> map fst
-    let ys = elves |> map snd
-    xs |> minimum, ys |> minimum, xs |> maximum, ys |> maximum
+    let rec round n elves dirs =
+        if n = 0 then elves else
+            let next = elves |> firstHalf dirs |> secondHalf
+            let dirs = (dirs |> skip 1) @ (dirs |> take 1)
+            round (n - 1) next dirs
 
-let vis elves =
-    use file = new System.IO.StreamWriter("vis23.txt")
-    let x1, y1, x2, y2 = extent elves
-    for y in y1 - 1 .. y2 + 1 do
-        for x in x1 - 1 .. x2 + 1 do
-            fprintf file "%c" (if elves |> Set.contains (x, y) then '#' else '.')
-        fprintfn file  ""
+    let countEmpty elves =
+        let xs = elves |> map fst
+        let ys = elves |> map snd
+        let x1, y1, x2, y2 = xs |> minimum, ys |> minimum, xs |> maximum, ys |> maximum
+        [ 
+            for y in y1 .. y2  do
+                for x in x1 .. x2 do
+                    x, y
+        ] |> List.filter ( fun p -> elves |> Set.contains p |> not ) |> length
 
-let rec round n elves dirs =
-    vis elves
-    if n = 0 then elves else
-    let next = elves |> firstHalf dirs |> secondHalf
-    let dirs = (dirs |> skip 1) @ (dirs |> take 1)
-    round (n - 1) next dirs
-
-let countEmpty elves =
-    let x1, y1, x2, y2 = extent elves
-    [ 
-        for y in y1 .. y2  do
-            for x in x1 .. x2 do
-                x, y
-    ] |> List.filter ( fun p -> elves |> Set.contains p |> not ) |> length
-
-round 10 elves [N; S; W; E] |> countEmpty
+    let partOne = round 10 elves [N; S; W; E] |> countEmpty
 
 
+module PartTwo =
+
+    let rec round n elves dirs =
+        let next = elves |> firstHalf dirs |> secondHalf
+        if next = elves then n else
+            let dirs = (dirs |> skip 1) @ (dirs |> take 1)
+            round (n + 1) next dirs
+
+    let partTwo = round 1 elves [N; S; W; E]
