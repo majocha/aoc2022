475dff9e19ca77426471b47d291c473dc3d0046a
majocha <majocha@outlook.com>
2022-12-24 10:09:40 +0100

    wip

--- day24.fsx
+++ day24.fsx
@@ -10,7 +10,7 @@ let inline (++) (x, y) (dx, dy) = x + dx, y + dy
 
 type BlizzardMap(a: int[,], d) =
     let dx, dy = d
-    let sizeX, sizeY = a |> Array2D.base1, a |> Array2D.base2
+    let sizeX, sizeY = a.GetLength 0, a.GetLength 1
     let wrap x size = if (x % size) < 0 then (x % size) + size else x % size
     let wrap2 (x, y) = wrap x sizeX, wrap y sizeY
     override _.ToString() = sprintf "%A" a
@@ -31,3 +31,20 @@ let pointAtTime =
     fun p t -> bs |> List.sumBy (fun b -> b.AtTime p t)
 
 
+let vis t =
+    use f = new System.IO.StreamWriter "vis24.txt"
+    for y in 0..sizeY - 1 do
+        for x in 0..sizeX - 1 do
+            let v = pointAtTime (x, y) t
+            fprintf f "%c" (if v = 0 then '.' elif v = 1 then '+' else '#')
+        fprintfn f ""
+
+
+let visn n =
+    for t in 1..n do
+        vis t
+        Async.Sleep 500 |> Async.RunSynchronously
+
+visn 10
+
+vis 0
